6314c054e564b55a96dee71b772b91bf9fdcb544
ItsMusicProject <usethrow064@gmail.com>
2025-03-30 08:47:09 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const continueButton = document.getElementById("continueButton");
+
+    continueButton.addEventListener("click", function () {
+        window.location.href = "mbti-gender-selection.html"; // Redirect to MBTI & Gender selection page
+    });
+});
